b6ca8e25a73194fb08f02bb9fcc487f222f8bce4
klopp <klopp@yandex.ru>
2016-08-11 00:47:32 +0300

    comments

--- btree.h
+++ btree.h
@@ -5,6 +5,10 @@
  *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
  */
 
+/*
+ * Balanced tree with numeric key
+ */
+
 #ifndef BTREE_H_
 #define BTREE_H_
 
@@ -23,7 +27,7 @@ typedef struct _BTNode {
     struct _BTNode *left;
 } *BTNode;
 
-typedef void (*BT_Walk)(BTNode node, void *data);
+typedef void ( *BT_Walk )( BTNode node, void *data );
 
 typedef struct _BTree {
     Tree_Flags flags;
@@ -32,20 +36,21 @@ typedef struct _BTree {
     BTNode head;
 } *BTree;
 
-BTree BT_create(Tree_Flags flags, Tree_Destroy destructor);
-void BT_clear(BTree tree);
-void BT_destroy(BTree tree);
-void BT_Free(void *data);
+BTree BT_create( Tree_Flags flags, Tree_Destroy destructor );
+void BT_clear( BTree tree );
+void BT_destroy( BTree tree );
+void BT_Free( void *data );
 
-size_t BT_depth(BTree tree);
+size_t BT_depth( BTree tree );
 
-BTNode BT_insert(BTree tree, TREE_KEY_TYPE key, void *data);
-int BT_delete(BTree tree, TREE_KEY_TYPE key);
-BTNode BT_search(BTree tree, TREE_KEY_TYPE key);
+BTNode BT_insert( BTree tree, TREE_KEY_TYPE key, void *data );
+int BT_delete( BTree tree, TREE_KEY_TYPE key );
+BTNode BT_search( BTree tree, TREE_KEY_TYPE key );
 
-void BT_walk(BTree tree, BT_Walk walker, void *data);
-void BT_walk_desc(BTree tree, BT_Walk walker, void *data);
-int BT_dump(BTree tree, Tree_KeyDump kdumper, Tree_DataDump ddumper, FILE *handle);
+void BT_walk( BTree tree, BT_Walk walker, void *data );
+void BT_walk_desc( BTree tree, BT_Walk walker, void *data );
+int BT_dump( BTree tree, Tree_KeyDump kdumper, Tree_DataDump ddumper,
+             FILE *handle );
 
 #ifdef __cplusplus
 }
--- stree.h
+++ stree.h
@@ -8,6 +8,10 @@
 #ifndef STREE_H_
 #define STREE_H_
 
+/*
+ * Splay tree with numeric key
+ */
+
 #include "tree.h"
 
 #ifdef __cplusplus
@@ -22,7 +26,7 @@ typedef struct _STNode {
     struct _STNode *right;
 } *STNode;
 
-typedef void (*ST_Walk)(STNode node, void *data);
+typedef void ( *ST_Walk )( STNode node, void *data );
 
 typedef struct _STree {
     Tree_Flags flags;
@@ -31,18 +35,19 @@ typedef struct _STree {
     STNode head;
 } *STree;
 
-STree ST_create(Tree_Flags flags, Tree_Destroy destructor);
-void ST_clear(STree tree);
-void ST_destroy(STree tree);
+STree ST_create( Tree_Flags flags, Tree_Destroy destructor );
+void ST_clear( STree tree );
+void ST_destroy( STree tree );
 
-size_t ST_depth(STree tree);
+size_t ST_depth( STree tree );
 
-STNode ST_insert(STree tree, TREE_KEY_TYPE key, void *data);
-int ST_delete(STree tree, TREE_KEY_TYPE key);
-STNode ST_search(STree tree, TREE_KEY_TYPE key);
+STNode ST_insert( STree tree, TREE_KEY_TYPE key, void *data );
+int ST_delete( STree tree, TREE_KEY_TYPE key );
+STNode ST_search( STree tree, TREE_KEY_TYPE key );
 
-void ST_walk(STree tree, ST_Walk walker, void *data);
-int ST_dump(STree tree, Tree_KeyDump kdumper, Tree_DataDump ddumper, FILE *handle);
+void ST_walk( STree tree, ST_Walk walker, void *data );
+int ST_dump( STree tree, Tree_KeyDump kdumper, Tree_DataDump ddumper,
+             FILE *handle );
 
 #ifdef __cplusplus
 }
