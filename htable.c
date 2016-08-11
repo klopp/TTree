2d1ae70aac2eaea8b81cdf786d3cef62642f47b6
klopp <klopp@yandex.ru>
2016-08-11 11:06:24 +0300

    HT_INTEGER_* added

--- htable.c
+++ htable.c
@@ -8,49 +8,74 @@
 
 HTable HT_create( Tree_Flags flags, Tree_Destroy destructor, size_t key_size )
 {
-    HTable ht = Malloc(sizeof(struct _HTable));
-    if( ht )
-    {
+    HTable ht = Malloc( sizeof( struct _HTable ) );
+
+    if( ht ) {
         ht->key_size = key_size;
-        ht->bt = BT_create(flags, destructor);
-        if( !ht->bt )
-        {
-            Free(ht);
+        ht->bt = BT_create( flags, destructor );
+
+        if( !ht->bt ) {
+            Free( ht );
             ht = NULL;
         }
     }
+
     return ht;
 }
 
 void HT_clear( HTable ht )
 {
-    BT_clear( ht->bt);
+    BT_clear( ht->bt );
 }
 void HT_destroy( HTable ht )
 {
-    BT_destroy(ht->bt);
-    Free(ht);
+    BT_destroy( ht->bt );
+    Free( ht );
 }
 
 unsigned int HT_set( HTable ht, void *key, void *data )
 {
-    unsigned int crc = crc32(key, ht->key_size);
+    unsigned int crc = crc32( key, ht->key_size );
     return BT_insert( ht->bt, crc, data ) ? crc : 0;
 }
 
-void *HT_get( HTable ht, void *key)
+void *HT_get( HTable ht, void *key )
 {
-    unsigned int crc = crc32(key, ht->key_size);
+    unsigned int crc = crc32( key, ht->key_size );
     BTNode btn = BT_search( ht->bt, crc );
     return btn ? btn->data : NULL;
 }
 
-int HT_delete( HTable ht, void *key)
+int HT_delete( HTable ht, void *key )
 {
-    unsigned int crc = crc32(key, ht->key_size);
+    unsigned int crc = crc32( key, ht->key_size );
     return BT_delete( ht->bt, crc );
 }
 
+#define HT_INTEGER_IMPL(tag,type) \
+    HTable HT_create_##tag( Tree_Flags flags, Tree_Destroy destructor ) { \
+        return HT_create( flags, destructor, sizeof(type) ); \
+    } \
+    unsigned int HT_set_##tag( HTable ht, type key, void *data ) { \
+        return HT_set( ht, &key, data ); \
+    } \
+    void *HT_get_##tag( HTable ht, type key) {; \
+        return HT_get( ht, &key ); \
+    } \
+    int HT_delete_##tag( HTable ht, type key) { \
+        return HT_delete( ht, &key ); \
+    }
+
+HT_INTEGER_IMPL( char, char );
+HT_INTEGER_IMPL( uchar, unsigned char );
+HT_INTEGER_IMPL( short, short );
+HT_INTEGER_IMPL( ushort, unsigned short );
+HT_INTEGER_IMPL( int, int );
+HT_INTEGER_IMPL( uint, unsigned int );
+HT_INTEGER_IMPL( long, long );
+HT_INTEGER_IMPL( ulong, unsigned long );
+HT_INTEGER_IMPL( llong, long long );
+HT_INTEGER_IMPL( ullong, unsigned long long );
 
 /*
  *  That's All, Folks!
--- htable.h
+++ htable.h
@@ -16,13 +16,12 @@
  */
 
 #if defined(__cplusplus)
-  extern "C" {
+extern "C" {
 #endif
 
-typedef struct _HTable
-{
-   BTree bt;
-   size_t key_size;
+typedef struct _HTable {
+    BTree bt;
+    size_t key_size;
 
 } *HTable;
 
@@ -31,11 +30,28 @@ void HT_clear( HTable ht );
 void HT_destroy( HTable ht );
 
 unsigned int HT_set( HTable ht, void *key, void *data );
-void *HT_get( HTable ht, void *key);
-int HT_delete( HTable ht, void *key);
+void *HT_get( HTable ht, void *key );
+int HT_delete( HTable ht, void *key );
+
+#define HT_INTEGER(tag,type) \
+        HTable HT_create_##tag( Tree_Flags flags, Tree_Destroy destructor ); \
+        unsigned int HT_set_##tag( HTable ht, type key, void *data ); \
+        void *HT_get_##tag( HTable ht, type key); \
+        int HT_delete_##tag( HTable ht, type key);
+
+HT_INTEGER( char, char );
+HT_INTEGER( uchar, unsigned char );
+HT_INTEGER( short, short );
+HT_INTEGER( ushort, unsigned short );
+HT_INTEGER( int, int );
+HT_INTEGER( uint, unsigned int );
+HT_INTEGER( long, long );
+HT_INTEGER( ulong, unsigned long );
+HT_INTEGER( llong, long long );
+HT_INTEGER( ullong, unsigned long long );
 
 #if defined(__cplusplus)
-  }; /* extern "C" */
+}; /* extern "C" */
 #endif
 
 #endif /* HTABLE_H_ */
